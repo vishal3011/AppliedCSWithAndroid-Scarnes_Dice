12433d4967c9a42f3a8d397604a55ea6a9f709a1
vishal3011 <thinkdifferent.vshal@gmail.com>
2016-11-01 15:17:36 +0530

    few updates

--- app/src/main/java/com/android/example/scarnesdice/MainActivity.java
+++ app/src/main/java/com/android/example/scarnesdice/MainActivity.java
@@ -1,10 +1,9 @@
 package com.android.example.scarnesdice;
 
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
